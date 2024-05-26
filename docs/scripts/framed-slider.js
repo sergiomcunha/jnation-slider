b7d52a7b031fc53e5fd35610a32c8423df1e0f0b
Sérgio Cunha <sergiomcunh@gmail.com>
2024-05-26 13:32:29 +0100

    chore: Update npm dependency to latest stable version

--- docs/scripts/framed-slider.js
+++ docs/scripts/framed-slider.js
@@ -1,24 +1,2 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35730/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
-var JNationFramedSlider = (function () {
-  'use strict';
-
-  var framedSlider = (function (frames) {
-    var sourceIndex = 0;
-    refreshFrame();
-    function refreshFrame() {
-      console.log("Changing frame source to ".concat(frames[sourceIndex].source));
-      document.querySelector('iframe').src = frames[sourceIndex].source;
-      window.setTimeout(function () {
-        sourceIndex++;
-        if (sourceIndex >= frames.length) {
-          sourceIndex = 0;
-        }
-        refreshFrame();
-      }, frames[sourceIndex].timeout);
-    }
-  });
-
-  return framedSlider;
-
-}());
+var JNationFramedSlider=function(){"use strict";return function(e){var o=0;!function n(){console.log("Changing frame source to ".concat(e[o].source)),document.querySelector("iframe").src=e[o].source,window.setTimeout((function(){++o>=e.length&&(o=0),n()}),e[o].timeout)}()}}();
+//# sourceMappingURL=framed-slider.js.map
--- docs/scripts/timed-slider.js
+++ docs/scripts/timed-slider.js
@@ -1,27 +1,2 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
-var JNationTimedSlider = (function () {
-  'use strict';
-
-  var timedSlider = (function (slots, timeout) {
-    var updateImage = function updateImage() {
-      var currentTime = new Date();
-      var currentTimeString = ('0' + currentTime.getHours()).slice(-2) + ':' + ('0' + currentTime.getMinutes()).slice(-2);
-      if (slots) {
-        for (var i = slots.length - 1; i >= 0; i--) {
-          if (currentTimeString >= slots[i].time) {
-            document.querySelector('main').style.backgroundImage = "url(\"../../images/".concat(slots[i].image, "\")");
-            break;
-          }
-        }
-      }
-    };
-    window.setInterval(function () {
-      updateImage();
-    }, timeout);
-    updateImage();
-  });
-
-  return timedSlider;
-
-}());
+var JNationTimedSlider=function(){"use strict";return function(e,t){var n=function(){var t=new Date,n=("0"+t.getHours()).slice(-2)+":"+("0"+t.getMinutes()).slice(-2);if(e)for(var i=e.length-1;i>=0;i--)if(n>=e[i].time){document.querySelector("main").style.backgroundImage='url("../../images/'.concat(e[i].image,'")');break}};window.setInterval((function(){n()}),t),n()}}();
+//# sourceMappingURL=timed-slider.js.map
